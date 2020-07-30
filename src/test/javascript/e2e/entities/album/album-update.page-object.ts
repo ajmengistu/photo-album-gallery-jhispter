a7c52dd6edbf64b561ddad43c9013d3a83c520bc
ajmengistu <35409325+ajmengistu@users.noreply.github.com>
2020-07-30 19:17:13 -0400

    Added initial jhipster entities from the jdl studio.

---
+++ src/test/javascript/e2e/entities/album/album-update.page-object.ts
@@ -0,0 +1,86 @@
+import { element, by, ElementFinder, protractor } from 'protractor';
+import { waitUntilDisplayed, waitUntilHidden, isVisible } from '../../util/utils';
+
+const expect = chai.expect;
+
+export default class AlbumUpdatePage {
+  pageTitle: ElementFinder = element(by.id('galleryApp.album.home.createOrEditLabel'));
+  saveButton: ElementFinder = element(by.id('save-entity'));
+  cancelButton: ElementFinder = element(by.id('cancel-save'));
+  titleInput: ElementFinder = element(by.css('input#album-title'));
+  descriptionInput: ElementFinder = element(by.css('textarea#album-description'));
+  createdInput: ElementFinder = element(by.css('input#album-created'));
+  userSelect: ElementFinder = element(by.css('select#album-user'));
+
+  getPageTitle() {
+    return this.pageTitle;
+  }
+
+  async setTitleInput(title) {
+    await this.titleInput.sendKeys(title);
+  }
+
+  async getTitleInput() {
+    return this.titleInput.getAttribute('value');
+  }
+
+  async setDescriptionInput(description) {
+    await this.descriptionInput.sendKeys(description);
+  }
+
+  async getDescriptionInput() {
+    return this.descriptionInput.getAttribute('value');
+  }
+
+  async setCreatedInput(created) {
+    await this.createdInput.sendKeys(created);
+  }
+
+  async getCreatedInput() {
+    return this.createdInput.getAttribute('value');
+  }
+
+  async userSelectLastOption() {
+    await this.userSelect.all(by.tagName('option')).last().click();
+  }
+
+  async userSelectOption(option) {
+    await this.userSelect.sendKeys(option);
+  }
+
+  getUserSelect() {
+    return this.userSelect;
+  }
+
+  async getUserSelectedOption() {
+    return this.userSelect.element(by.css('option:checked')).getText();
+  }
+
+  async save() {
+    await this.saveButton.click();
+  }
+
+  async cancel() {
+    await this.cancelButton.click();
+  }
+
+  getSaveButton() {
+    return this.saveButton;
+  }
+
+  async enterData() {
+    await waitUntilDisplayed(this.saveButton);
+    await this.setTitleInput('title');
+    expect(await this.getTitleInput()).to.match(/title/);
+    await waitUntilDisplayed(this.saveButton);
+    await this.setDescriptionInput('description');
+    expect(await this.getDescriptionInput()).to.match(/description/);
+    await waitUntilDisplayed(this.saveButton);
+    await this.setCreatedInput('01/01/2001' + protractor.Key.TAB + '02:30AM');
+    expect(await this.getCreatedInput()).to.contain('2001-01-01T02:30');
+    await this.userSelectLastOption();
+    await this.save();
+    await waitUntilHidden(this.saveButton);
+    expect(await isVisible(this.saveButton)).to.be.false;
+  }
+}
